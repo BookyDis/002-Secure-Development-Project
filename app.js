b2d169a79a97d1821ada18cba9b90ded5c35dff3
Oliver Stopher <oliver@stopher.co.uk>
2025-04-12 13:47:20 +0100

    CSRF Fixes (Fixed Correct import from csrf to double csrf) + Undefined remaining cookie options removed.
    Created .gitignore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 //csrf-csrf library requirement
 const cookieParser = require('cookie-parser');
-const csrf = require('csrf-csrf');
+const { doubleCsrf } = require('csrf-csrf'); //Fixed Correct import from csrf to double csrf
 
 //app intialisation
 const app = express();
@@ -18,37 +18,37 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 //home route
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index", {
         title: "Home"
     });
 });
 
 //adding other routes
-app.get('/postsPage', function(req, res){
+app.get('/postsPage', function (req, res) {
     res.render('postsPage', {
-        title:'Posts'
+        title: 'Posts'
     })
 })
 
 //start server
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
 
 //function to create csrf
 const doubleCsrfUtilities = doubleCsrf({
-  getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
-  getSessionIdentifier: (req) => "", // A function that should return the session identifier for a given request
-  cookieName: "__Host-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
-  cookieOptions: {
-    sameSite : "lax",  // Recommend you make this strict if posible
-    path : "/",
-    secure : true,
-    ...remainingCookieOptions // See cookieOptions below
-  },
-  size: 64, // The size of the generated tokens in bits
-  ignoredMethods: ["GET", "HEAD", "OPTIONS"], // A list of request methods that will not be protected.
-  getTokenFromRequest: (req) => req.headers["x-csrf-token"], // A function that returns the token from the request
+    getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
+    getSessionIdentifier: (req) => "", // A function that should return the session identifier for a given request
+    cookieName: "__Host-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
+    cookieOptions: {
+        sameSite: "lax",  // Recommend you make this strict if posible
+        path: "/",
+        secure: true
+    },
+    size: 64, // The size of the generated tokens in bits
+    ignoredMethods: ["GET", "HEAD", "OPTIONS"], // A list of request methods that will not be protected.
+    getTokenFromRequest: (req) => req.headers["x-csrf-token"], // A function that returns the token from the request
 });
+
 
